b2eee1de0a21921a85233a6bef085f7be21c1ab2
Muhammad Aditya <aditya210muhammad@gmail.com>
2018-03-07 17:31:45 +0800

    starbase not done yet

--- applications/starbase/src/App.vue
+++ applications/starbase/src/App.vue
@@ -1,16 +1,27 @@
 <template>
   <div id="app">
     <h3>{{ title }}</h3>
+    <div class="col-md-12">
+      <Character
+        v-for="(id, index) in initial_ids"
+        :id="id"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import Character from './components/Character.vue'
 export default {
   name: 'app',
   data() {
     return {
-      title : 'Generate Your Team'
+      title : 'Generate Your Team',
+      initial_ids : [1,13,14]
     }
+  },
+  components: {
+    Character
   }
 }
 </script>
