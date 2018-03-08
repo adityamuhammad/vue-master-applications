c10031626f4aec27d4d782f179c2a8cbf39e967f
Muhammad Aditya <aditya210muhammad@gmail.com>
2018-03-08 12:41:15 +0800

    Setted up for signing using firebase

---
+++ applications/turnout/src/firebaseApp.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyCmx5u9F1QAxnK4vrDbtWxdXRDBeN2iDQg",
+  authDomain: "turnout-ecac1.firebaseapp.com",
+  databaseURL: "https://turnout-ecac1.firebaseio.com",
+  projectId: "turnout-ecac1",
+  storageBucket: "turnout-ecac1.appspot.com",
+  messagingSenderId: "245959422668"
+};
+
+export const firebaseApp = firebase.initializeApp(config)
