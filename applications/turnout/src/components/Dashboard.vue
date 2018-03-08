5a13f5dda91b9d7d501601379de7e0b30a8187c4
Muhammad Aditya <aditya210muhammad@gmail.com>
2018-03-08 20:47:57 +0800

    Add submit event features

--- applications/turnout/src/components/Dashboard.vue
+++ applications/turnout/src/components/Dashboard.vue
@@ -3,13 +3,25 @@
     Dashboard Component
     <h3>Events Dashboard</h3>
     <button class="btn btn-danger btn-sm signout-btn" @click="signOut">SignOut</button>
-
-    {{ $store.state }}
+    <hr>
+    <AddEvent
+      
+    />
+    <hr>
+    <div class="col-md-12">
+      <EventItem
+        v-for="(event_item, index) in this.$store.state.events"
+        :event="event_item"
+        key="index"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import { firebaseApp } from '../firebaseApp'
+import { firebaseApp, eventsRef } from '../firebaseApp'
+import AddEvent from './AddEvent.vue'
+import EventItem from './EventItem.vue'
 
 export default {
   methods : {
@@ -17,6 +29,19 @@ export default {
       this.$store.dispatch('signOut')
       firebaseApp.auth().signOut()
     }
+  },
+  components: {
+    AddEvent,
+    EventItem
+  },
+  mounted() {
+    eventsRef.on('value', snap => {
+      let events = []
+      snap.forEach(event => {
+        events.push(event.val())
+      })
+      this.$store.dispatch('setEvents', events)
+    })
   }
 }
 </script>
